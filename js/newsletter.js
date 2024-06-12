b815c5ec01faf8dc32455bee36881aac8c34124b
Ralph Carvalho <123739852+RA-L-PH@users.noreply.github.com>
2024-06-12 11:51:27 +0530

    newsletter

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,32 +1,19 @@
-const submitButton = document.getElementById("submitButton");
-const emailInput = document.getElementById("newsletteremail");
-
-submitButton.addEventListener("click", function() {
-
-    // Replace with your deployed Apps Script web app URL (ensure it's HTTPS)
-    const url = 'https://script.google.com/macros/s/AKfycbzS_Grd1xcKCXu_hYPnEQnHQdU6_zKVCMEn9r9kYtzJzj-i42TKZB5T7GIRqhbjsYnBZA/exec';
-
-    fetch(url, {
-        redirect: "follow",
-        method: "POST",
-        body: JSON.stringify({
-            "email" : emailInput.value
-        }),
-        headers: {
-            'Content-Type' : "text/plain;charset=utf-8"
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.status === 200) {
-            alert("Email added successfully!");
-            emailInput.value = ""; // Clear the input field
-        } else {
-            alert("Error adding email: " + data.code);
-        }
-    })
-    .catch(error => {
-        console.error(error);
-        alert("An error occurred. Please try again later.");
-    });
-});
+const msg = document.getElementById('msg');
+window.addEventListener("DOMContentLoaded", function() {
+  const yourForm = document.getElementById('FORM_ID');
+  yourForm.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(yourForm);
+      const action = e.target.action;
+      fetch(action, {
+          method: 'POST',
+          body: data,
+      }).then(response => {
+        msg.innerHTML = "Thank you for Subscribing!"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        },5000)
+      })
+      .catch(error => console.error('Error', error))
+  })
+}); 
